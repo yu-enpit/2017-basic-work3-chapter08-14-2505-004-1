d193ddf69887f131b32fcca51dab53d778278821
yu-enpit27 <u004ff@gmail.com>
2017-08-23 09:31:22 +0900

    cp8

---
+++ MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/MySchaedulerApplication.java
@@ -0,0 +1,21 @@
+package com.example.yu_enpit.myscheduler;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by Prof.Hamamoto on 2017/08/04.
+ */
+
+public class MySchaedulerApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();;
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfig);
+
+    }
+}
